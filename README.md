09e66713b651dbb9c9c3f32d89717d735cd9a629
Conor Malone <connyfromtheblock@gmail.com>
2024-10-20 21:14:49 +0100

    finished shift & mim direction, adding target count next

--- README.md
+++ README.md
@@ -2,14 +2,18 @@
 NFL Big Data Bowl 2025
 
 to do:
+filter to only pass or RPO plays
 collect observations
   def org
   off from
-  
-get binary motion vars
 
-shift: stops before snap, do left, same side, right
-man in motion: going at snap, get direction of movement (towards ball)
+get counts of targets in previous weeks/to date
+
+maybe
+get binary motion vars
 whether corners follow (maybe use player vars)
 get metric of openness
 
+done
+shift: stops before snap, do left, same side, right
+man in motion: going at snap, get direction of movement (towards ball)
--- initial_cleaning.R
+++ initial_cleaning.R
@@ -13,6 +13,7 @@ week1 <- read.csv("tracking_week_1.csv")
 #week9 <- read.csv("tracking_week_9.csv")
 tracking_dirty <- rbind(week1)#,week2,week3,week4,week5,week6,week7,week8,week9)
 rm(week1)#,week2,week3,week4,week5,week6,week7,week8,week9)
+#####################
 #preprocess tracking data
 pre_processing_function <- function(df_input){
   dataframe <- df_input %>% 
@@ -40,12 +41,37 @@ dataframe$comb_id <- as.factor(dataframe$comb_id)
 dataframe$gameId <- as.factor(dataframe$gameId)
 dataframe$playId <- as.factor(dataframe$playId)
 dataframe$event <- as.factor(dataframe$event)
-rm(list=c("play_track","play_clean","player_play_clean","tracking_clean","plays","player_play"))
+dataframe$passResult <- as.factor(dataframe$passResult)
+#just pass or run pass option plays
+dataframe <- dataframe %>% filter((pff_runPassOption==1) | (passResult !=""))
+
+rm(list=c("play_track","play_clean","player_play_clean","tracking_clean"))
+#end preprocess
+###########################################
+#feature 1
+#direction of shift & man in motion
 movers_and_shakers <- dataframe %>% filter((inMotionAtBallSnap==1|shiftSinceLineset==1|motionSinceLineset==1))
 plays_with_motion <- dataframe %>% filter(inMotionAtBallSnap==1)%>% select(c(comb_id,nflId)) %>% unique()
 plays_with_shift <- dataframe %>% filter(shiftSinceLineset==1) %>% select(c(comb_id,nflId)) %>% unique()
 just_mim_tracking <- dataframe %>%  filter(comb_id %in% plays_with_motion$comb_id & nflId %in% plays_with_motion$nflId)
 just_shift_tracking <- dataframe %>%  filter(comb_id %in% plays_with_shift$comb_id & nflId %in% plays_with_shift$nflId )
+#get direction of travel of man in motion
+start_mim <- just_mim_tracking %>% filter(event=="man_in_motion")%>% select(c(comb_id, frameId,nflId,y_std)) %>% rename( start_y =y_std)
+end_mim <- just_mim_tracking %>% filter(event=="ball_snap")%>% select(c(comb_id, frameId,nflId,y_std)) %>% rename( end_y =y_std)
+#merge start and end mim, get diff, if larger they move right, if smaller left
+mim_dir <- start_mim %>% merge(., end_mim, by=c("comb_id","nflId")) %>% mutate(y_diff = start_y - end_y) %>% 
+  select(-c(frameId.x,frameId.y,start_y,end_y)) %>% filter(abs(y_diff) >0.25) %>% 
+  mutate(motion_dir = ifelse(y_diff>0,"Right","Left")) %>% select(-y_diff)
+#get direction of travel of shift
+start_shift <- just_shift_tracking %>% filter(event=="shift")%>% select(c(comb_id, frameId,nflId,y_std)) %>% rename( start_y =y_std)
+end_shift <- just_shift_tracking %>% filter(event=="ball_snap")%>% select(c(comb_id, frameId,nflId,y_std)) %>% rename( end_y =y_std)
+#merge start and end shift, get diff, if larger they move right, if smaller left
+shift_dir <- start_shift %>% merge(., end_shift, by=c("comb_id","nflId")) %>% mutate(y_diff = start_y - end_y) %>% 
+  select(-c(frameId.x,frameId.y,start_y,end_y)) %>% filter(abs(y_diff) >0.1) %>% 
+  mutate(motion_dir = ifelse(y_diff>0,"Right","Left")) %>% select(-y_diff)   
+######################################
+#feature2
+#target metrics to rank players by most popular (HMM starts with most pop)
+#import games to get week counts
+games <-read.csv("games.csv")
 
-start_mim <- just_mim_tracking %>% filter(event=="man_in_motion")%>% select(c(comb_id, frameId,nflId,y_std))
-end_mim <- just_mim_tracking %>% filter(event=="ball_snap")%>% select(c(comb_id, frameId,nflId,y_std))
