811efc188e70cf147c4dddec7a31aaea8cc4b121
Conor Malone <connyfromtheblock@gmail.com>
2024-11-27 08:58:11 +0000

    having completed modelling, inc predictions

--- mixed-hmm.r
+++ mixed-hmm.r
@@ -3,7 +3,7 @@ library(depmixS4)
 library(tidyverse)
 # Example data - replace this with your actual dataframe
 # Ensure your data is preprocessed and has these columns as factors
-df <- train_features #%>% filter(week==2) # %>% select(c(defendersInBox,
+df <- train_features #%>% filter(week<4) # %>% select(c(defendersInBox,
                      #              personnelID, speed_s_1))# Replace with your actual data
 
 # Convert columns to factors
@@ -11,7 +11,7 @@ factor_columns <- c( "down","yardsToGo","score_bucket","pff_manZone","defendersI
                     "personnelD", "speed_s_1", "speed_s_2", "speed_s_3", "speed_s_4", "speed_s_5","bucket_lag_1",
                     "bucket_lag_2","bucket_lag_3","bucket_lag_4","bucket_lag_5")
 
-df[factor_columns] <- lapply(df[factor_columns], as.factor)
+#df[factor_columns] <- lapply(df[factor_columns], as.factor)
 
 # Define the number of states
 n_states <- 5
@@ -32,13 +32,12 @@ hmm_model <- depmix(list( down ~ 1, yardsToGo ~ 1, score_bucket ~ 1,
                           pff_manZone ~ 1,  defendersInBox ~ 1, 
                           personnelD ~ 1, speed_s_1 ~ 1, speed_s_2 ~ 1, speed_s_3 ~ 1, speed_s_4 ~ 1, speed_s_5 ~ 1,bucket_lag_1 ~ 1,
                           bucket_lag_2 ~ 1,bucket_lag_3 ~ 1,bucket_lag_4 ~ 1,bucket_lag_5 ~ 1),
-                    data = df, nstates = n_states, family = rep(list(multinomial()), length(factor_columns)),ntimes=ntimes,
-                    trstart = as.vector(trans_matrix), instart=as.vector(trans_matrix[1,]))
-
+                    data = df, nstates = n_states, family = rep(list(multinomial()), length(factor_columns)),ntimes=ntimes
+)
 
 
 # Fit the model
-fitted_model <- fit(hmm_model, emcontrol = em.control(maxit = 5, tol = 1e-8), verbose = TRUE)
+fitted_model <- fit(hmm_model, emcontrol = em.control(maxit = 10, tol = 1e-8), verbose = TRUE)
 
 
 # Summary of the fitted model
@@ -48,17 +47,22 @@ getpars(fitted_model)
 
 state_probs <- posterior(fitted_model)
 train_features$preds <-state_probs$state
-#write.csv(train_features,"train_preds.csv")
+df <- df %>%
+  group_by(comb_id) %>%                           # Group by comb_id
+  mutate(last_frame = ifelse(frameId == max(frameId), 1, 0)) %>%  # Create binary marker
+  ungroup()   
+write.csv(df,"train_preds.csv")
 # Create a new depmix model structure for the unseen data
 hmm_new <- depmix(
   response = list(down ~ 1, yardsToGo ~ 1, score_bucket ~ 1, 
                   pff_manZone ~ 1,  defendersInBox ~ 1, 
                   personnelD ~ 1, speed_s_1 ~ 1, speed_s_2 ~ 1, speed_s_3 ~ 1, speed_s_4 ~ 1, speed_s_5 ~ 1,bucket_lag_1 ~ 1,
                   bucket_lag_2 ~ 1,bucket_lag_3 ~ 1,bucket_lag_4 ~ 1,bucket_lag_5 ~ 1),
-  data = test_features,
+  data = combined_all_features,
   nstates = fitted_model@nstates,  # Use the same number of states
   family = rep(list(multinomial()), length(factor_columns)),  # Same family
-  ntimes = nrow(test_features)  # Adjust for new data length
+  ntimes = nrow(combined_all_features),
+  trstart = as.vector(trans_matrix), instart=as.vector(trans_matrix[1,])# Adjust for new data length
 )
 
 hmm_new <- setpars(hmm_new, getpars(fitted_model))
@@ -66,15 +70,9 @@ validObject(hmm_new)
 # Adjust parameter lengths
 state_probs_new <- posterior(hmm_new)
 predicted_states_new <- state_probs_new$state
-test_features$preds <-predicted_states_new
-#write.csv(test_features,"test_preds.csv")
-trained_params <- getpars(fitted_model)
-new_params <- getpars(hmm_new)
-
-# Compare the first few parameters
-head(trained_params)
-head(new_params)
-setdiff(trained_params,new_params)
-# Compare parameter names or indices if possible
-print(names(trained_params))
-print(names(new_params))
+combined_all_features$preds <-predicted_states_new
+test_preds <- combined_all_features %>% filter(week==9) %>% 
+  group_by(comb_id) %>%                           # Group by comb_id
+  mutate(last_frame = ifelse(frameId == max(frameId), 1, 0)) %>%  # Create binary marker
+  ungroup()   
+write.csv(test_preds,"test_preds2.csv")
